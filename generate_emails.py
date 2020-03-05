70db205a61a5742a001e4a69b6598aef48715a9a
abhijit-c <abhijit9331@gmail.com>
2020-03-05 15:30:37 -0500

    Done.

--- generate_emails.py
+++ generate_emails.py
@@ -26,24 +26,24 @@ for k in range(num_people):
 
     if not is_profiles_generated:
         os.system('wget -P ./profiles/ ' + URL) 
-    if basename == '':
+    if basename == '' or not os.path.exists('./profiles/' + basename):
         failed_read.append(data[k]['Name'])
         continue
 
     with open('./profiles/' + basename, 'r') as f:
         prof = [line.strip() for line in f]
         coded_email = [x for x in prof if '&#' in x]
+        e = ''
         if len(coded_email) != 0:
-            print(
-                    strip_htmltags(
-                        html.unescape(
-                            strip_htmltags(coded_email[0])
-                        ).strip()
-                    )
-                )
-        else: #maybe uncoded
+            e = strip_htmltags(
+                html.unescape(
+                    strip_htmltags(coded_email[0])
+                ).strip()
+            )
+        elif len([x for x in prof if '@' in x]) != 0: #maybe uncoded
             email = [x for x in prof if '@' in x][0]
-            print(strip_htmltags(email))
+            e = strip_htmltags(email)
+        print(data[k]['Name'].split(',')[0] + ', ' + e)
 
 print('Failed to automatically read the following profiles:')
 pprint.pprint(failed_read)
